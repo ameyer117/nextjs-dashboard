35a8fbc9cf9e80ef86b1e51962db483e7caeafd1
Alec Meyer <11meyal@gmail.com>
2023-11-14 11:06:02 -0500

    Ready for db

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+
+    return (
+        <div className="container mx-auto py-10">
+            <h1 className="text-3xl text-blue-600 font-bold">Invoices</h1>
+        </div>
+    )
+}
+
+
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Page() {
+
+    return (
+        <div className="container mx-auto py-10">
+            <h1 className="text-3xl text-blue-600 font-bold">Dashboard</h1>
+        </div>
+    )
+}
+
+
